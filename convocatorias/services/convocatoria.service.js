7b457c840978e18f67d7d5fb87ede581c77d3e14
Cristian Narvaez <crissnar12xd@gmail.com>
2025-05-20 02:56:14 -0500

    [FIX]: fix in update convocatorias service

--- convocatorias/services/convocatoria.service.js
+++ convocatorias/services/convocatoria.service.js
@@ -1,8 +1,8 @@
-import ExcelJS from 'exceljs';
-import { Convocatoria } from '../../models/Convocatoria.js';
+import ExcelJS from "exceljs";
+import { Convocatoria } from "../../models/Convocatoria.js";
 
 export const getAllConvocatorias = async () => {
-  return await Convocatoria.find().populate('user_id');
+  return await Convocatoria.find().populate("user_id");
 };
 
 export const createConvocatoria = async (data, userId) => {
@@ -13,7 +13,7 @@ export const createConvocatoria = async (data, userId) => {
   const nuevaConvocatoria = new Convocatoria({
     ...data,
     user_id: userId,
-    diferencia_presupuesto: diferenciaPresupuesto
+    diferencia_presupuesto: diferenciaPresupuesto,
   });
 
   return await nuevaConvocatoria.save();
@@ -21,18 +21,23 @@ export const createConvocatoria = async (data, userId) => {
 
 export const updateConvocatoria = async (id, data) => {
   const convocatoria = await Convocatoria.findById(id);
-  if (!convocatoria) throw new Error('Convocatoria not found');
+  if (!convocatoria) throw new Error("Convocatoria not found");
 
-  Object.assign(convocatoria, data); // actualiza campos existentes
+  Object.assign(convocatoria, data);
+
+  const valorSolicitado = parseFloat(convocatoria.valor_solicitado) || 0;
+  const valorAprobado = parseFloat(convocatoria.valor_aprobado) || 0;
+  convocatoria.diferencia_presupuesto = valorSolicitado - valorAprobado;
+
   return await convocatoria.save();
 };
 
 export const deleteConvocatoria = async (id) => {
   const convocatoria = await Convocatoria.findById(id);
-  if (!convocatoria) throw new Error('Convocatoria not found');
+  if (!convocatoria) throw new Error("Convocatoria not found");
 
   await convocatoria.deleteOne();
-  return { message: 'Convocatoria deleted' };
+  return { message: "Convocatoria deleted" };
 };
 
 export const filterConvocatorias = async (filters) => {
@@ -40,28 +45,28 @@ export const filterConvocatorias = async (filters) => {
 
   for (const key in filters) {
     if (filters[key]) {
-      query[key] = { $regex: filters[key], $options: 'i' };
+      query[key] = { $regex: filters[key], $options: "i" };
     }
   }
 
-  return await Convocatoria.find(query).populate('user_id');
+  return await Convocatoria.find(query).populate("user_id");
 };
 
 export const generateConvocatoriasReport = async (data) => {
   const workbook = new ExcelJS.Workbook();
-  const worksheet = workbook.addWorksheet('Convocatorias');
+  const worksheet = workbook.addWorksheet("Convocatorias");
 
   if (data.length === 0) {
-    worksheet.addRow(['No data available']);
+    worksheet.addRow(["No data available"]);
   } else {
-    const columns = Object.keys(data[0]._doc).map(key => ({
+    const columns = Object.keys(data[0]._doc).map((key) => ({
       header: key,
       key: key,
       width: 20,
     }));
     worksheet.columns = columns;
 
-    data.forEach(row => {
+    data.forEach((row) => {
       worksheet.addRow(row._doc);
     });
   }
@@ -70,7 +75,7 @@ export const generateConvocatoriasReport = async (data) => {
 };
 
 export const getConvocatoriaById = async (id) => {
-  const convocatoria = await Convocatoria.findById(id).populate('user_id');
-  if (!convocatoria) throw new Error('Convocatoria not found');
+  const convocatoria = await Convocatoria.findById(id).populate("user_id");
+  if (!convocatoria) throw new Error("Convocatoria not found");
   return convocatoria;
 };
